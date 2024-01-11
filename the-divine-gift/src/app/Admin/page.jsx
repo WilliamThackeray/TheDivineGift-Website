f9d6453ccf447557b5e902e498a6376ea28312fa
William Thackeray <will_thack@comcast.net>
2024-01-10 17:34:04 -0700

    Added Admin page

---
+++ the-divine-gift/src/app/Admin/page.jsx
@@ -0,0 +1,9 @@
+export default function Admin() {
+  return (
+    <main className={styles.admin}>
+      <h1 className={styles.title}>
+        <a href="/">Admin</a>
+      </h1>
+    </main>
+  )
+}
